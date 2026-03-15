6dc987c1b1fbc673c5d786ac2cce0ccedbcb5521
Zakhar Guzii <zakharguzii@gmail.com>
2026-03-15 17:21:14 +0200

    add normalization for random forest

--- task_1/scr/random_forest_model.py
+++ task_1/scr/random_forest_model.py
@@ -1,18 +1,16 @@
-from task_1.scr.interface import MnistClassifierInterface
 from sklearn.ensemble import RandomForestClassifier
+
+from task_1.scr.interface import MnistClassifierInterface
+
 
 class RandomForestModel(MnistClassifierInterface):
     def __init__(self):
         self._model = RandomForestClassifier(n_estimators=100)
-    
-    def _prepoces(self, X):
-        X_norm = X/255
-        return X_norm
-    
+
     def train(self, X_train, y_train):
-        X_train_norm = self._prepoces(X_train)
-        self._model.fit(X_train_norm, y_train)
+        X_flat_train = X_train.reshape(len(X_train), -1) / 255.0
+        self._model.fit(X_flat_train, y_train)
 
     def predict(self, X_test):
-        X_test_norm = self._prepoces(X_test)
-        return self._model.predict(X_test_norm)
+        X_flat_test = X_test.reshape(len(X_test), -1) / 255.0
+        return self._model.predict(X_flat_test)
